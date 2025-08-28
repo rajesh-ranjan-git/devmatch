08a15324b51c72294ca43889e7d0f7cc171c70c4
Rajesh Ranjan <rajesh.ranjan@aajtak.com>
2025-08-28 12:40:14 +0530

    db setup with type errors

---
+++ devmatch-brainbox/db/connectDB.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+
+// Load environment variables
+const envFile =
+  process.env.NODE_ENV === "production" ? ".env-production" : ".env-local";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), "env", envFile),
+});
+
+const DB_URI = process.env.DB_URI;
+const DB_NAME = process.env.DB_NAME;
+
+if (!DB_URI || !DB_NAME) {
+  throw new Error("DB CONFIGURATION NOT FOUND");
+}
+
+const connectDB = async () => {
+  console.log("Connecting DevMatch database...");
+
+  try {
+    await mongoose.connect(`${DB_URI}/${DB_NAME}`);
+    console.log("DEVMATCH DB CONNECTED");
+  } catch (error) {
+    console.error(
+      `ERROR :: DB connection failed with message : ${error.message}`
+    );
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- devmatch-brainbox/src/server.ts
+++ devmatch-brainbox/src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import path from "path";
 
+import connectDB from "../db/connectDB.ts";
+
 // Determine environment
 const envFile =
   process.env.NODE_ENV === "production" ? ".env-production" : ".env-local";
@@ -9,13 +11,25 @@ const envFile =
 // Load the .env file from env folder
 dotenv.config({ path: path.resolve(process.cwd(), "env", envFile) });
 
-const BRAINBOX_PORT = process.env.BRAINBOX_PORT;
-const BRAINBOX_HOST_URL = process.env.BRAINBOX_HOST_URL;
+const BRAINBOX_PORT = process.env.BRAINBOX_PORT || 7000;
+const BRAINBOX_HOST_URL =
+  process.env.BRAINBOX_HOST_URL || "http://localhost:7000/";
 
 const server = express();
 
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: `Server is running at ${BRAINBOX_HOST_URL} on PORT : ${BRAINBOX_PORT}`,
+  });
+});
+
 server.listen(BRAINBOX_PORT, () => {
-  console.log(
-    `Server is running at ${BRAINBOX_HOST_URL} on PORT : ${BRAINBOX_PORT}`
-  );
+  connectDB().then(() => {
+    console.log(
+      `Server is running at ${BRAINBOX_HOST_URL} on PORT : ${BRAINBOX_PORT}`
+    );
+  });
 });
