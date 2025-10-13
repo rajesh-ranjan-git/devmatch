79cff9a54f0898c241c6f799353a14638da0f919
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-13 16:28:51 +0530

    navbar

--- devmatch-visualcortex/src/app/page.tsx
+++ devmatch-visualcortex/src/app/page.tsx
@@ -5,12 +5,19 @@ export default function Home() {
   return (
     <div className="justify-items-center items-center gap-16 grid grid-rows-[20px_1fr_20px] p-8 sm:p-20 pb-20 min-h-screen font-sans">
       <main className="flex flex-col items-center sm:items-start gap-[32px] row-start-2">
-        <p className="flex justify-center items-center gap-2 font-mono text-primary text-6xl sm:text-left text-center list-decimal list-inside">
-          <Image
+        <p className="flex justify-center items-center gap-2 font-mono text-primary text-3xl sm:text-left text-center list-decimal list-inside">
+          {/* <Image
             src={"/devmatch-logo.svg"}
             width={80}
             height={80}
             alt="dev-match-logo"
+          /> */}
+          <Image
+            src={"/devmatch-logo.png"}
+            width={40}
+            height={40}
+            alt="dev-match-logo"
+            className="border-2 border-primary rounded-full"
           />
           <span>DevMatch</span>
         </p>
--- devmatch-visualcortex/src/components/navbar/navbar.tsx
+++ devmatch-visualcortex/src/components/navbar/navbar.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
-const navbar = () => {
-  return <div></div>;
+const Navbar = () => {
+  return (
+    <section className="flex justify-between items-center p-2 px-20 border-primary-foreground border-b-2 w-full text-primary text-2xl">
+      <div className="flex justify-center items-center gap-2 font-semibold">
+        <Image
+          src={"/devmatch-logo.png"}
+          width={40}
+          height={40}
+          alt="dev-match-logo"
+          className="border-2 border-primary rounded-full"
+        />
+        <span>DevMatch</span>
+      </div>
+      <div>
+        <Avatar className="cursor-pointer">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </section>
+  );
 };
 
-export default navbar;
+export default Navbar;
