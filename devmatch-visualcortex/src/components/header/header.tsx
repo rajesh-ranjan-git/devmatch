e99c459615f320938aa3d0e003b9ae85f3204a62
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-14 18:15:30 +0530

    devmatch logo finalized

--- devmatch-visualcortex/src/components/header/header.tsx
+++ devmatch-visualcortex/src/components/header/header.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
     <header className="flex justify-center items-center pt-4 pb-2 w-screen max-w-screen h-20 overflow-x-hidden font-bold text-3xl">
       <div className="flex justify-center items-center glass-border border-1 w-full h-full glass-lg container">
-        DevMatch
+        <div className="w-16 h-16 object-cover">
+          <Image
+            src={"/assets/logo/devmatch-logo-transparent-circular.webp"}
+            width={200}
+            height={200}
+            alt="devmatch-logo"
+          />
+        </div>
+        <span>DevMatch</span>
       </div>
     </header>
   );
--- devmatch-visualcortex/src/components/main/main.tsx
+++ devmatch-visualcortex/src/components/main/main.tsx
@@ -1,11 +1,20 @@
 import DefaultAnimatedBackground from "@/components/background/defaultAnimatedBackground";
+import Image from "next/image";
 
 const main = () => {
   return (
     <main className="flex-1 justify-center items-center w-screen max-w-screen h-full overflow-x-hidden font-semibold text-lg">
       <DefaultAnimatedBackground />
-      <div className="flex justify-center items-center pb-10 w-full h-full container">
-        DevMatch is here!
+      <div className="flex flex-col justify-center items-center gap-4 pb-10 w-full h-full container">
+        <div className="w-24 h-24 object-cover">
+          <Image
+            src={"/assets/logo/devmatch-logo-white-circular.webp"}
+            width={200}
+            height={200}
+            alt="devmatch-logo"
+          />
+        </div>
+        <span className="text-3xl">DevMatch</span>
       </div>
     </main>
   );
