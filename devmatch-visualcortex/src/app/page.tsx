40735a7dbf168c55f940b361fe4630691dfebb1b
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-14 10:39:26 +0530

    confusion confusion

--- devmatch-visualcortex/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col justify-between items-center py-4 w-screen h-screen max-h-screen container">
-      <section>Navbar</section>
-      <section>DevMatch</section>
-      <section>DevFooterMatch</section>
-    </div>
-  );
-}
