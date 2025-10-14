93945d987cfb84d3e414fb6090d0baec03de42ee
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-14 16:49:16 +0530

    main default background

--- devmatch-visualcortex/src/app/layout.tsx
+++ devmatch-visualcortex/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased  max-h-screen h-screen overflow-hidden glass-bg`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  max-h-screen h-screen overflow-hidden glass-bg dark`}
         suppressHydrationWarning
       >
         {children}
--- devmatch-visualcortex/src/app/page.tsx
+++ devmatch-visualcortex/src/app/page.tsx
@@ -1,19 +1,22 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <header className="glass-border border-b-2 w-screen max-w-screen h-16 overflow-x-hidden font-bold text-3xl glass">
-        <div className="flex justify-center items-center w-full h-full container">
+      <header className="flex justify-center items-center pt-4 w-screen max-w-screen h-20 overflow-x-hidden font-bold text-3xl">
+        <div className="flex justify-center items-center glass-border border-2 w-full h-full glass container">
           DevMatch
         </div>
       </header>
       <main className="flex-1 justify-center items-center w-screen max-w-screen h-full overflow-x-hidden font-semibold text-lg">
+        <div className="blob-outer-container">
+          <div className="blob-inner-container">
+            <div className="blob"></div>
+          </div>
+        </div>
         <div className="flex justify-center items-center pb-10 w-full h-full container">
           DevMatch
         </div>
       </main>
-      <footer className="bottom-0 shadow-bottom fixed glass-border border-t-2 w-screen max-w-screen h-10 overflow-x-hidden font-semibold text-xl">
+      <footer className="bottom-0 shadow-bottom fixed glass-border border-t-2 rounded-t-lg w-screen max-w-screen h-10 overflow-x-hidden font-semibold text-xl">
         <div className="flex justify-center items-center w-full h-full container">
           DevMatch
         </div>
