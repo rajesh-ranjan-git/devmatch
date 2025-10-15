e2ecfb731227b21c3c1c1df9cfc945afd8210aef
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-15 11:45:09 +0530

    refactor

--- devmatch-visualcortex/src/components/ui/dropdown-menu.tsx
+++ devmatch-visualcortex/src/components/ui/dropdown-menu.tsx
@@ -33,7 +33,7 @@ function DropdownMenuTrigger({
 
 function DropdownMenuContent({
   className,
-  sideOffset = 12,
+  sideOffset = 6,
   ...props
 }: React.ComponentProps<typeof DropdownMenuPrimitive.Content>) {
   return (
