8a03c45be5f63a1e5c880f5b35d303f19430ae71
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-25 17:42:41 +0530

    smaller theme toggle

--- devmatch-visualcortex/src/components/theme/themeToggle.tsx
+++ devmatch-visualcortex/src/components/theme/themeToggle.tsx
@@ -29,12 +29,12 @@ const ThemeToggle = () => {
       />
       <label
         htmlFor="theme-toggle"
-        className="block after:top-0.5 after:left-0.5 peer-checked:after:left-20 after:absolute relative bg-[#242424] after:bg-[linear-gradient(180deg,#ffcc89,#d8860b)] peer-checked:after:bg-[linear-gradient(180deg,#777,#3a3a3a)] peer-checked:bg-[#ebebeb] shadow-[inset_0_5px_15px_rgba(0,0,0,0.4),inset_0_-5px_15px_rgba(255,255,255,0.4)] after:shadow-black/20 after:shadow-lg rounded-4xl after:rounded-full w-20.5 after:w-7 active:after:w-10 h-8 after:h-7 after:content-[''] transition-all after:transition-all peer-checked:after:-translate-x-full duration-500 after:duration-500 cursor-pointer"
+        className="block after:top-0.5 after:left-0.5 peer-checked:after:left-15.5 after:absolute relative bg-[#242424] after:bg-[linear-gradient(180deg,#ffcc89,#d8860b)] peer-checked:after:bg-[linear-gradient(180deg,#777,#3a3a3a)] peer-checked:bg-[#ebebeb] shadow-[inset_0_5px_15px_rgba(0,0,0,0.4),inset_0_-5px_15px_rgba(255,255,255,0.4)] after:shadow-black/20 after:shadow-lg rounded-4xl after:rounded-full w-16 after:w-5 active:after:w-10 h-6 after:h-5 after:content-[''] transition-all after:transition-all peer-checked:after:-translate-x-full duration-500 after:duration-500 cursor-pointer"
       >
         <svg
-          className={`top-1.5 left-1.5 z-50 absolute ${
+          className={`top-1 left-1 z-50 absolute ${
             switchTheme === "dark" ? "fill-[#7e7e7e]" : "fill-white"
-          } w-5 transition-all duration-500`}
+          } w-4 transition-all duration-500`}
           viewBox="0 0 24 24"
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
@@ -51,9 +51,9 @@ const ThemeToggle = () => {
           />
         </svg>
         <svg
-          className={`top-1.5 left-14 z-50 absolute ${
+          className={`top-1 left-11 z-50 absolute ${
             switchTheme === "dark" ? "fill-white" : "fill-[#7e7e7e]"
-          } w-5 transition-all duration-500`}
+          } w-4 transition-all duration-500`}
           viewBox="0 0 24 24"
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
