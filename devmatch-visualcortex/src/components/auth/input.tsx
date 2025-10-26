84f867b647985bc65dfb4b547ea90f515bbebca4
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-10-26 19:30:46 +0530

    input text color

--- devmatch-visualcortex/src/components/auth/input.tsx
+++ devmatch-visualcortex/src/components/auth/input.tsx
@@ -9,7 +9,7 @@ const Input = ({
     <input
       type={type}
       placeholder={placeholder}
-      className="bg-glass-surface-heavy shadow-glass-shadow-heavy shadow-md mt-5 px-5 py-2.5 border border-glass-border-bright border-r-glass-border-subtle border-b-glass-border-subtle border-none rounded-4xl outline-none w-full text-md text-white placeholder:text-glass-text-secondary tracking-wider"
+      className="bg-glass-surface-heavy shadow-glass-shadow-heavy shadow-md mt-5 px-5 py-2.5 border border-glass-border-bright border-r-glass-border-subtle border-b-glass-border-subtle border-none rounded-4xl outline-none w-full text-glass-text-primary text-md placeholder:text-glass-text-secondary tracking-wider"
     />
   );
 };
