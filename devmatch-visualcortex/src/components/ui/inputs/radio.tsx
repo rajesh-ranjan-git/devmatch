f0715c2d5d2b886c5519bfa5d5d4728de94d56c0
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-01-16 05:38:30 +0530

    radio ui update

--- devmatch-visualcortex/src/components/ui/inputs/radio.tsx
+++ devmatch-visualcortex/src/components/ui/inputs/radio.tsx
@@ -9,11 +9,15 @@ const Radio = ({ name, options, value, className, icon }: RadioProps) => {
 
   return (
     <div
-      className={`flex justify-between items-center px-4 py-2 w-full overflow-hidden text-glass-text-primary text-md tracking-wider  ${className}`}
+      className={`flex justify-between items-center w-full text-glass-text-primary text-md tracking-wider select-none ${className}`}
     >
-      <div className="flex items-center gap-8">
+      <div className="flex items-center gap-8 py-2">
         {options?.map((option, idx) => (
-          <div key={idx} className="flex items-center gap-2">
+          <div
+            key={idx}
+            className="flex items-center gap-2 bg-glass-surface-heavy shadow-glass-shadow-heavy shadow-md px-4 py-2 border border-glass-border-bright border-r-glass-border-subtle border-b-glass-border-subtle border-none rounded-4xl outline-none w-full min-w-fit overflow-hidden text-glass-text-primary text-md placeholder:text-glass-text-secondary cursor-pointer"
+            onClick={() => setUpdatedValue(option)}
+          >
             <input
               name={name}
               id={option}
@@ -21,13 +25,17 @@ const Radio = ({ name, options, value, className, icon }: RadioProps) => {
               checked={updatedValue === option}
               value={updatedValue}
               onChange={() => setUpdatedValue(option)}
-              className="px-5 py-2 rounded-l-4xl outline-none w-full"
+              className="px-5 py-2 rounded-l-4xl outline-none w-full cursor-pointer"
             />
-            <label htmlFor={option}>{toTitleCase(option)}</label>
+            <label htmlFor={option} className="cursor-pointer">
+              {toTitleCase(option)}
+            </label>
           </div>
         ))}
       </div>
-      {icon}
+      <span className="bg-glass-surface-heavy shadow-glass-shadow-heavy shadow-md mr-2 p-2 border border-glass-border-bright border-r-glass-border-subtle border-b-glass-border-subtle border-none rounded-full outline-none min-w-fit overflow-hidden text-glass-text-primary">
+        {icon}
+      </span>
     </div>
   );
 };
