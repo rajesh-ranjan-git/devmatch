9e2db0efbcd1f2470252d4b81eb0a94435aadb9c
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-01-28 11:46:19 +0530

    improved context ui

--- devmatch-visualcortex/src/components/ui/contextMenu/contextMenu.tsx
+++ devmatch-visualcortex/src/components/ui/contextMenu/contextMenu.tsx
@@ -22,19 +22,28 @@ const ContextMenu = ({
   if (!isVisible) return null;
 
   return (
-    <div
-      className={`top-1/2 left-1/2 z-10 absolute bg-glass-surface-light shadow-glass-shadow-heavy shadow-md backdrop-blur-md p-2 border rounded-xl min-w-80 min-h-40 overflow-hidden text-glass-text-primary text-center transition-all -translate-x-1/2 -translate-y-1/2 animate-[fadeIn_0.5s_ease-in-out,slideInFromBottom_0.5s_ease-in-out] duration-500 ease-in-out ${
-        open ? "opacity-100" : "opacity-0"
-      } ${className ? className : ""}`}
-    >
-      <button
-        className="top-1 right-1 absolute p-1 border border-glass-border-subtle hover:border-glass-border-bright rounded-md text-sm cursor-pointer"
+    <>
+      <div
+        className={`fixed inset-0 bg-black/80 z-50 transition-opacity duration-500 animate-[fadeIn_0.5s_ease-in-out] ${
+          open ? "opacity-100" : "opacity-0"
+        }`}
         onClick={onClose}
+      />
+
+      <div
+        className={`top-1/2 left-1/2 z-50 fixed bg-glass-surface-light shadow-glass-shadow-heavy shadow-md backdrop-blur-md p-2 border rounded-xl min-w-80 min-h-40 overflow-hidden text-glass-text-primary text-center transition-all -translate-x-1/2 -translate-y-1/2 animate-[fadeIn_0.5s_ease-in-out,slideInFromBottom_0.5s_ease-in-out] duration-500 ease-in-out ${
+          open ? "opacity-100" : "opacity-0"
+        } ${className ? className : ""}`}
       >
-        <ImCross size={10} />
-      </button>
-      {children || "Context Menu"}
-    </div>
+        <button
+          className="top-1 right-1 absolute p-1 border border-glass-border-subtle hover:border-glass-border-bright rounded-md text-sm cursor-pointer"
+          onClick={onClose}
+        >
+          <ImCross size={10} />
+        </button>
+        {children || "Context Menu"}
+      </div>
+    </>
   );
 };
 
