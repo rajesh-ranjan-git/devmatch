d330c9c28060f797137824c5fe2fab6f1fb165fc
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-01-30 13:29:11 +0530

    input css update

--- devmatch-visualcortex/src/components/ui/inputs/input.tsx
+++ devmatch-visualcortex/src/components/ui/inputs/input.tsx
@@ -14,7 +14,7 @@ const Input = ({
 }: InputProps) => {
   return (
     <div
-      className={`flex items-center gap-2 bg-glass-surface-heavy shadow-glass-shadow-heavy shadow-md pr-4 border border-glass-border-bright border-r-glass-border-subtle border-b-glass-border-subtle border-none rounded-4xl outline-none w-full overflow-hidden text-glass-text-primary text-md placeholder:text-glass-text-secondary tracking-wider ${className}`}
+      className={`flex items-center gap-2 bg-glass-surface-light shadow-glass-shadow-heavy shadow-md pr-4 border border-glass-border-bright border-r-glass-border-subtle border-b-glass-border-subtle border-none rounded-4xl outline-none w-full overflow-hidden text-glass-text-primary text-md placeholder:text-glass-text-secondary tracking-wider ${className}`}
     >
       <input
         name={name}
@@ -24,7 +24,7 @@ const Input = ({
         defaultValue={defaultValue as string}
         onChange={onChange}
         onKeyDown={onKeyDown}
-        className="px-5 py-2 rounded-l-4xl outline-none w-full"
+        className="focus:bg-glass-surface-heavy px-5 py-2 rounded-l-4xl outline-none w-full transition-all ease-in-out caret-glass-text-primary"
       />
       <span onClick={onClick} className={onClick ? "cursor-pointer" : ""}>
         {icon}
