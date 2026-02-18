e5a22042de1131def9534e0dc8f1467fb29d6fea
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-02-18 14:00:16 +0530

    socket full configuration server-side

--- devmatch-brainbox/socket/socket.js
+++ devmatch-brainbox/socket/socket.js
@@ -3,6 +3,8 @@ import {
   BRAINBOX_HOST_URL,
   VISUALCORTEX_HOST_URL,
 } from "../config/constants.js";
+import Conversation from "../models/conversation.js";
+import Message from "../models/message.js";
 import { getSecretRoomId } from "../utils/utils.js";
 
 export const initializeSocket = (server) => {
@@ -12,7 +14,18 @@ export const initializeSocket = (server) => {
     },
   });
 
+  const onlineUsers = new Map();
+  const typingUsers = new Map();
+
   io.on("connection", (socket) => {
+    const userId = socket.handshake.query?.userId;
+
+    if (userId) {
+      onlineUsers.set(userId, socket.id);
+
+      io.emit("onlineUsers", Array.from(onlineUsers.keys()));
+    }
+
     socket.on("joinChat", ({ userId, targetUserId }) => {
       const roomId = getSecretRoomId([userId, targetUserId]);
 
@@ -22,13 +35,93 @@ export const initializeSocket = (server) => {
     socket.on("sendMessage", ({ userId, targetUserId, message }) => {
       const roomId = getSecretRoomId([userId, targetUserId]);
 
-      io.to(roomId).emit("receivedMessage", {
-        text: message,
-        sentBy: userId,
-        sentAt: new Date().toISOString(),
+      io.to(roomId).emit("receivedMessage", message);
+    });
+
+    socket.on("editMessage", ({ userId, targetUserId, messageId, newText }) => {
+      const roomId = getSecretRoomId([userId, targetUserId]);
+
+      io.to(roomId).emit("messageEdited", { messageId, newText });
+    });
+
+    socket.on("deleteMessage", ({ userId, targetUserId, messageId }) => {
+      const roomId = getSecretRoomId([userId, targetUserId]);
+
+      io.to(roomId).emit("messageDeleted", { messageId });
+    });
+
+    socket.on("joinGroupChat", ({ conversationId }) => {
+      socket.join(conversationId);
+    });
+
+    socket.on("leaveGroupChat", ({ conversationId }) => {
+      socket.leave(conversationId);
+    });
+
+    socket.on("sendGroupMessage", ({ conversationId, message }) => {
+      io.to(conversationId).emit("receivedGroupMessage", message);
+    });
+
+    socket.on(
+      "editGroupMessage",
+      ({ conversationId, messageId, newMessage }) => {
+        io.to(conversationId).emit("groupMessageEdited", {
+          messageId,
+          newMessage,
+        });
+      },
+    );
+
+    socket.on("deleteGroupMessage", ({ conversationId, messageId }) => {
+      io.to(conversationId).emit("groupMessageDeleted", { messageId });
+    });
+
+    socket.on("typing", ({ userId, targetUserId }) => {
+      const roomId = getSecretRoomId([userId, targetUserId]);
+
+      socket.to(roomId).emit("userTyping", { userId });
+    });
+
+    socket.on("stopTyping", ({ userId, targetUserId }) => {
+      const roomId = getSecretRoomId([userId, targetUserId]);
+
+      socket.to(roomId).emit("userStoppedTyping", { userId });
+    });
+
+    socket.on("groupTyping", ({ userId, conversationId }) => {
+      if (!typingUsers.has(conversationId)) {
+        typingUsers.set(conversationId, new Set());
+      }
+      typingUsers.get(conversationId).add(userId);
+
+      socket.to(conversationId).emit("usersTyping", {
+        conversationId,
+        typingUserIds: Array.from(typingUsers.get(conversationId)),
       });
     });
 
-    socket.on("disconnect", ({ userId, targetUserId }) => {});
+    socket.on("groupStopTyping", ({ userId, conversationId }) => {
+      typingUsers.get(conversationId)?.delete(userId);
+
+      socket.to(conversationId).emit("usersTyping", {
+        conversationId,
+        typingUserIds: Array.from(typingUsers.get(conversationId) || []),
+      });
+    });
+
+    socket.on("disconnect", () => {
+      typingUsers.forEach((users, conversationId) => {
+        if (users.has(userId)) {
+          users.delete(userId);
+          io.to(conversationId).emit("usersTyping", {
+            conversationId,
+            typingUserIds: Array.from(users),
+          });
+        }
+      });
+
+      onlineUsers.delete(userId);
+      io.emit("onlineUsers", Array.from(onlineUsers.keys()));
+    });
   });
 };
