b4e0e6d009cd44e37fca4131077886acf8e84885
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-02-18 12:35:40 +0530

    working on chat controllers

--- devmatch-brainbox/controllers/conversation.js
+++ devmatch-brainbox/controllers/conversation.js
@@ -6,6 +6,7 @@ import {
 } from "../config/config.js";
 import { ValidationError } from "../errors/CustomError.js";
 import Conversation from "../models/conversation.js";
+import Message from "../models/message.js";
 import { isValidMongoDbObjectId } from "../utils/authUtils.js";
 import { sanitizeMongoData } from "../utils/utils.js";
 import { limitValidator, pageValidator } from "../validations/validation.js";
@@ -92,7 +93,7 @@ export const chatMessages = async (req, res) => {
 
     const validatedLimit = limitValidator(limit);
 
-    let conversation = await Conversation.find({
+    let conversation = await Conversation.findOne({
       participants: { $all: [loggedInUserId, otherUserId] },
     })
       .populate({
@@ -106,16 +107,20 @@ export const chatMessages = async (req, res) => {
       .limit(validatedLimit || 10);
 
     if (!conversation) {
-      conversation = await Conversation.create({
-        participants: [loggedInUserId, otherUserId],
+      return res.status(status.success.statusCode).json({
+        status: status.success.message,
+        statusCode: status.success.statusCode,
+        data: {
+          conversation: null,
+        },
+        message: successMessages.FETCH_MESSAGES_SUCCESS,
       });
-    } else {
     }
 
     const sanitizedConversation = sanitizeMongoData(conversation);
 
-    const totalCount = await Conversation.countDocuments({
-      participants: [loggedInUserId, otherUserId],
+    const totalMessageCount = await Message.countDocuments({
+      conversationId: sanitizedConversation?.id,
     });
 
     return res.status(status.success.statusCode).json({
@@ -124,90 +129,7 @@ export const chatMessages = async (req, res) => {
       data: {
         conversation: sanitizedConversation,
         pagination: {
-          total: totalCount || "",
-          page: validatedPage || "",
-          limit: validatedLimit || "",
-          totalPages:
-            totalCount && validatedLimit
-              ? Math.ceil(totalCount / validatedLimit)
-              : "",
-        },
-      },
-      message: successMessages.FETCH_MESSAGES_SUCCESS,
-    });
-  } catch (error) {
-    return res
-      .status(
-        error?.status?.statusCode || status.internalServerError.statusCode,
-      )
-      .json({
-        status: error?.status?.message || status.internalServerError.message,
-        statusCode:
-          error?.status?.statusCode || status.internalServerError.statusCode,
-        apiUrl: error?.apiUrl || req?.url,
-        error: {
-          type: error?.type,
-          message: error?.message,
-          data: error?.data,
-        },
-      });
-  }
-};
-
-export const groupChatMessages = async (req, res) => {
-  try {
-    const { id: otherUserId } = await req?.params;
-
-    const { id: loggedInUserId } = req?.data;
-
-    if (otherUserId && !isValidMongoDbObjectId(otherUserId)) {
-      throw new ValidationError(
-        status.forbidden,
-        errorMessages.INVALID_NOTIFICATION_ID_FORMAT_ERROR,
-        { otherUserId },
-        req?.url,
-      );
-    }
-
-    const { page, limit } = await req?.query;
-
-    const validatedPage = pageValidator(page);
-
-    const validatedLimit = limitValidator(limit);
-
-    let conversation = await Conversation.find({
-      participants: { $all: [loggedInUserId, otherUserId] },
-    })
-      .populate({
-        path: "messages",
-        populate: {
-          path: "senderId",
-          select: Object.values(defaultUserProperties),
-        },
-      })
-      .skip(((validatedPage || 1) - 1) * (validatedLimit || 10))
-      .limit(validatedLimit || 10);
-
-    if (!conversation) {
-      conversation = await Conversation.create({
-        participants: [loggedInUserId, otherUserId],
-      });
-    } else {
-    }
-
-    const sanitizedConversation = sanitizeMongoData(conversation);
-
-    const totalCount = await Conversation.countDocuments({
-      participants: [loggedInUserId, otherUserId],
-    });
-
-    return res.status(status.success.statusCode).json({
-      status: status.success.message,
-      statusCode: status.success.statusCode,
-      data: {
-        conversation: sanitizedConversation,
-        pagination: {
-          total: totalCount || "",
+          total: totalMessageCount || "",
           page: validatedPage || "",
           limit: validatedLimit || "",
           totalPages:
--- devmatch-brainbox/models/message.js
+++ devmatch-brainbox/models/message.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const messageSchema = mongoose.Schema(
   {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+    },
     senderId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
